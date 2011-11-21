8d3a6d78f2c13d18d26eda2b0ec874deac38a2ba
Rohland de Charmoy <rohland.decharmoy@gmail.com>
2011-11-21 20:40:18 +0200

    added project files without history (from Codeplex project) due to issues with codeplex migration

---
+++ Demo/Default.aspx
@@ -0,0 +1,24 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Default.aspx.cs" Inherits="Demo._Default" %>
+
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+
+<html xmlns="http://www.w3.org/1999/xhtml" >
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+    <div>
+    <h2>Old Text</h2>
+    <hr />
+    <asp:Literal runat="server" ID="litOldText"></asp:Literal>
+    <h2>New Text</h2>
+    <hr />
+    <asp:Literal runat="server" ID="litNewText"></asp:Literal>
+    <h2>Html Diff Visualisation</h2>
+    <hr />
+    <asp:Literal runat="server" ID="litDiffText"></asp:Literal>
+    </div>
+    </form>
+</body>
+</html>
---
+++ Demo/Default.aspx.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using Helpers;
+
+namespace Demo
+{
+    public partial class _Default : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            string oldText = @"<p><i>This is</i> some sample text to <strong>demonstrate</strong> the capability of the <strong>HTML diff tool</strong>.</p>
+                                <p>It is based on the <b>Ruby</b> implementation found <a href='http://github.com/myobie/htmldiff'>here</a>. Note how the link has no tooltip</p>
+                                <table cellpadding='0' cellspacing='0'>
+                                <tr><td>Some sample text</td><td>Some sample value</td></tr>
+                                <tr><td>Data 1 (this row will be removed)</td><td>Data 2</td></tr>
+                                </table>
+                                Here is a number 2 32";
+
+            string newText = @"<p>This is some sample <strong>text to</strong> demonstrate the awesome capabilities of the <strong>HTML <u>diff</u> tool</strong>.</p><br/><br/>Extra spacing here that was not here before.
+                                <p>It is <i>based</i> on the Ruby implementation found <a title='Cool tooltip' href='http://github.com/myobie/htmldiff'>here</a>. Note how the link has a tooltip now and the HTML diff algorithm has preserved formatting.</p>
+                                <table cellpadding='0' cellspacing='0'>
+                                <tr><td>Some sample <strong>bold text</strong></td><td>Some sample value</td></tr>
+                                </table>
+                                Here is a number 2 <sup>32</sup>";
+
+            HtmlDiff diffHelper = new HtmlDiff(oldText, newText);
+            litOldText.Text = oldText;
+            litNewText.Text = newText;
+            litDiffText.Text = diffHelper.Build();
+        }
+    }
+}
