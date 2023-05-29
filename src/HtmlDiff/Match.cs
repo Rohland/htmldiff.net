4a594f5a33aa2739482575b4946e30559e800f59
Rohland de Charmoy <rohland@codeo.co.za>
2023-05-29 15:12:30 +0200

    :bug: refactoring and addressing bug with block globbing

--- src/HtmlDiff/Match.cs
+++ src/HtmlDiff/Match.cs
@@ -1,4 +1,3 @@
-using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
 
@@ -6,42 +5,34 @@ namespace HtmlDiff
 {
     public class Match
     {
-        public Match(int startInOld, int startInNew, int size)
+        public Match(
+            int startInOld,
+            int startInNew,
+            int size)
         {
             StartInOld = startInOld;
             StartInNew = startInNew;
             Size = size;
         }
 
-        public int StartInOld { get; set; }
-        public int StartInNew { get; set; }
-        public int Size { get; set; }
-
-        public int EndInOld
-        {
-            get { return StartInOld + Size; }
-        }
-
-        public int EndInNew
-        {
-            get { return StartInNew + Size; }
-        }
-
+        public int StartInOld { get; }
+        public int StartInNew { get; }
+        public int Size { get; }
+        public int EndInOld => StartInOld + Size;
+        public int EndInNew => StartInNew + Size;
 
 #if DEBUG
-
         public void PrintWordsFromOld(string [] oldWords)
         {
-            var text = string.Join("", oldWords.Where((s, pos) => pos >= this.StartInOld && pos < this.EndInOld).ToArray());
+            var text = string.Join("", oldWords.Where((s, pos) => pos >= StartInOld && pos < EndInOld).ToArray());
             Debug.WriteLine("OLD: " + text);
         }
 
         public void PrintWordsFromNew(string [] newWords)
         {
-            var text = string.Join("", newWords.Where((s, pos) => pos >= this.StartInNew && pos < this.EndInNew).ToArray());
+            var text = string.Join("", newWords.Where((s, pos) => pos >= StartInNew && pos < EndInNew).ToArray());
             Debug.WriteLine("NEW: " + text);
         }
-
 #endif
     }
 }
